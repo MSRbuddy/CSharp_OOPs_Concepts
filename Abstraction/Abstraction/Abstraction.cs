77f1a72b248ff1e1e8a051a9e24c0855d0a6cca8
MSRbuddy <meghanadasari222@gmail.com>
2023-04-02 16:46:53 +0530

    Abstraction in C#!

---
+++ Abstraction/Abstraction/Abstraction.cs
@@ -0,0 +1,27 @@
+﻿namespace Abstraction
+{
+    abstract class Book
+    {
+        // abstract method
+        public abstract void bookSubject();
+    }
+
+    // inheriting from abstract class
+    class TextBook : Book
+    {
+        // provide implementation of abstract method
+        public override void bookSubject()
+        {
+            Console.WriteLine("English");
+        }
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // create an object of Dog class
+            TextBook obj = new TextBook();
+            obj.bookSubject();
+        }
+    }
+}
