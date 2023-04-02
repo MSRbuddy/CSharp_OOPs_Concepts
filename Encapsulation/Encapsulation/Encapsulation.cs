cff01f67a70fa459e6b87aa681c9e0d8e736b1d9
MSRbuddy <meghanadasari222@gmail.com>
2023-04-02 16:45:29 +0530

    Encapsulation in C#!

---
+++ Encapsulation/Encapsulation/Encapsulation.cs
@@ -0,0 +1,48 @@
+﻿namespace Encapsulation
+{
+    public class Bank
+    {
+        private int Amount;
+        public int GetAmount()
+        {
+            return Amount;
+        }
+        public void SetAmount(int Amount)
+        {
+            if (Amount > 0)
+            {
+                this.Amount = Amount;
+            }
+            else
+            {
+                throw new Exception("Please Pass a Positive Value");
+            }
+        }
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                Bank bank = new Bank();
+                
+                //We cannot access the Amount Variable directly
+                //bank.Amount = 50; //Compile Time Error
+                //Console.WriteLine(bank.Amount); //Compile Time Error
+                
+                //Setting Positive Value
+                bank.SetAmount(25000);
+                Console.WriteLine(bank.GetAmount());
+                
+                //Setting Negative Value
+                bank.SetAmount(-150);
+                Console.WriteLine(bank.GetAmount());
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+    }
+}
