e205fd5ff506ba70722b94a1277ca0520a15441a
MSRbuddy <meghanadasari222@gmail.com>
2023-04-02 16:34:23 +0530

    Sample Programs of OOPs concepts in C#!

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace SampleOOPsConcepts
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Sample OOPs concepts!");
+        }
+    }
+}
