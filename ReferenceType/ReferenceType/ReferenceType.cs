294fad9500d5d879b2bb7bcde4e7025debf5d49a
MSRbuddy <meghanadasari222@gmail.com>
2023-04-02 16:51:41 +0530

    ValueType and ReferenceType in C#!

---
+++ ReferenceType/ReferenceType/ReferenceType.cs
@@ -0,0 +1,21 @@
+﻿namespace ReferenceType
+{
+    internal class Student
+    {
+        string Name;
+        static void ChangeReferenceType(Student std2)
+        {
+            std2.Name = "Meghana Dasari";
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine("*****ReferenceType in C#*****");
+            Student std1 = new Student();
+            std1.Name = "Meghana";
+
+            ChangeReferenceType(std1);
+
+            Console.WriteLine(std1.Name);
+        }
+    }
+}
---
+++ ValueType/ValueType/ValueType.cs
@@ -0,0 +1,21 @@
+﻿namespace ValueType
+{
+    internal class Program
+    {
+        static void ChangeValue(int x)
+        {
+            x = 200;
+            Console.WriteLine(x);
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine("*****ValueType in C#*****");
+            
+            int i = 100;
+            Console.WriteLine(i);
+
+            ChangeValue(i);
+            Console.WriteLine(i);
+        }
+    }
+}
