ca8352f20c02fb057ddbfab0d208b2341b594c29
MSRbuddy <meghanadasari222@gmail.com>
2023-04-02 16:41:46 +0530

    Inheritance in C#!

---
+++ Inheritance/Inheritance/Inheritance.cs
@@ -0,0 +1,29 @@
+﻿namespace Inheritance
+{
+    class A
+    {
+        public void Method1()
+        {
+            Console.WriteLine("Method 1: Hi");
+        }
+        public void Method2()
+        {
+            Console.WriteLine("Method 2: Hello");
+        }
+    }
+    class B : A
+    {
+        public void Method3()
+        {
+            Console.WriteLine("Method 3: Everyone");
+        }
+        static void Main()
+        {
+            B obj = new B();
+            obj.Method1();
+            obj.Method2();
+            obj.Method3();
+            Console.ReadKey();
+        }
+    }
+}
