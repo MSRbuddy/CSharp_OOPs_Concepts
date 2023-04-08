d67965120eeb1127fed814bc0403ab111f3981be
MSRbuddy <meghanadasari222@gmail.com>
2023-04-08 22:18:20 +0530

    Inheritance-Types in C#!

---
+++ TypesOfInheritance/TypesOfInheritance/Inheritance.cs
@@ -0,0 +1,41 @@
+﻿namespace TypesOfInheritance
+{
+    internal class Inheritance
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("**********Types of Inheritance in C-Sharp**********");
+            Console.WriteLine("\n");
+
+            Console.WriteLine("*****Single Inheritance*****");
+            TextBook tb = new TextBook();
+            tb.Read("TB-1", 6);
+            tb.ReadTextBook();
+            Console.WriteLine("\n");
+
+            Console.WriteLine("*****Multi-level Inheritance*****");
+            Physics ph = new Physics();
+            ph.Read("TB-2", 7);
+            ph.ReadMathsBook();
+            ph.ReadPhysicsBook();
+            Console.WriteLine("\n");
+
+            Console.WriteLine("*****Hierarchical Inheritance*****");
+            Botany bt = new Botany();
+            bt.Read("TB-3", 8);
+            bt.ReadBiologyBook();
+            bt.ReadBotanyBook();
+
+            Zoology zg = new Zoology();
+            zg.Read("TB-4", 9);
+            zg.ReadBiologyBook();
+            zg.ReadZoologyBook();
+            Console.WriteLine("\n");
+
+            Console.WriteLine("*****Multiple Inheritance*****");
+            Language ln = new Language();
+            ln.ReadTeluguBook();
+            ln.ReadHindiBook();
+        }
+    }
+}
---
+++ TypesOfInheritance/TypesOfInheritance/InheritanceTypes.cs
@@ -0,0 +1,86 @@
+﻿namespace TypesOfInheritance
+{
+        //Single-Inheritance
+        class Book
+        {
+            public string Title;
+            public int Standard;
+            public void Read(string name,int standard)
+            {
+                Title= name;
+                Standard= standard;
+                Console.WriteLine("This is a Book: {0} of class: {1}",name,standard);
+            }
+        }
+        class TextBook : Book
+        {
+            public void ReadTextBook()
+            {
+                Console.WriteLine("This is a TextBook.");
+            }
+        }
+
+        //Multi-level Inheritance
+        class Maths : Book
+        {
+            public void ReadMathsBook()
+            {
+                Console.WriteLine("This is a Maths Book.");
+            }
+        }
+        class Physics : Maths
+        {
+            public void ReadPhysicsBook()
+            {
+                Console.WriteLine("This is a Physics Book.");
+            }
+        }
+
+        //Hierarchical Inheritance
+        class Biology : Book
+        {
+            public void ReadBiologyBook()
+            {
+                Console.WriteLine("This is a Biology Book.");
+            }
+        }
+        class Botany : Biology
+        {
+            public void ReadBotanyBook()
+            {
+                Console.WriteLine("This is a Botany Book.");
+            }
+        }
+        class Zoology : Biology
+        {
+            public void ReadZoologyBook()
+            {
+                Console.WriteLine("This is a Zoology Book.");
+            }
+        }
+
+        //Multiple Inheritance
+        interface ITelugu
+        {
+            public void ReadTeluguBook()
+            { 
+            }
+        }
+        interface IHindi
+        {
+            public void ReadHindiBook()
+            {
+            }
+        }
+        class Language : ITelugu,IHindi
+        {
+            public void ReadTeluguBook()
+            {
+                Console.WriteLine("This is a Telugu Book");
+            }
+            public void ReadHindiBook()
+            {
+                Console.WriteLine("This is a Hindi Book");
+            }
+        }
+}
